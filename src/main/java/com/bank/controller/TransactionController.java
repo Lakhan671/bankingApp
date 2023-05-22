1835993732977919a5e884a9c1df2e59080a637c
Lakhan671 <lakhans671@gmail.com>
2023-05-22 15:06:02 +0530

    remove testing code

--- src/main/java/com/bank/controller/TransactionController.java
+++ src/main/java/com/bank/controller/TransactionController.java
@@ -11,11 +11,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
 import java.util.WeakHashMap;
 import java.util.concurrent.ExecutionException;
+
 /**
  * This class is used to withdraw and deposit money into a bank account.
-@author Lakhan Singh
+ *
+ * @author Lakhan Singh
  */
 @RestController
 @RequestMapping(value = "/api")
@@ -23,16 +26,11 @@ import java.util.concurrent.ExecutionException;
 @CrossOrigin(origins = "*")
 public class TransactionController {
     @Autowired
-   private TransactionService transactionService;
-    @GetMapping("/resource/user")
-    @PreAuthorize("hasRole('USER')")
-    public Mono<ResponseEntity<Message>> user() {
-        return Mono.just(ResponseEntity.ok(new Message("Content for user")));
-    }
+    private TransactionService transactionService;
 
     @PreAuthorize("hasRole('USER')")
     @PostMapping(value = "/transaction/save/v1")
-    public Mono<ResponseEntity<ResponseData>> addTransaction(@RequestBody WeakHashMap<String,String> request) throws ExecutionException, InterruptedException {
+    public Mono<ResponseEntity<ResponseData>> addTransaction(@RequestBody WeakHashMap<String, String> request) throws ExecutionException, InterruptedException {
         log.info(" Add transaction  request  ={}", request);
         return transactionService.saveTransaction(request)
                 .doOnSuccess(response -> log.info(" request for transaction={} completed successfully", response))
@@ -42,7 +40,7 @@ public class TransactionController {
                 .onErrorResume(e -> {
                     Throwable throwable = (Throwable) e;
                     log.info("Transaction with error {}", throwable.getMessage());
-                    return   Mono.just(ResponseEntity
+                    return Mono.just(ResponseEntity
                             .accepted()
                             .body(ResponseData.builder().msg("There is a problem with transaction processing; either the bank's server is down or the services are unavailable.").build()));
                 });
@@ -52,17 +50,17 @@ public class TransactionController {
 
     @PreAuthorize("hasRole('USER')")
     @PostMapping(value = "/transaction/get/v1")
-    public Flux<Transaction> getTransaction(@RequestBody WeakHashMap<String,String> request) {
+    public Flux<Transaction> getTransaction(@RequestBody WeakHashMap<String, String> request) {
         log.info(" getTransaction method request ={}", request);
-        return  transactionService.getTransactionHistory(request);
+        return transactionService.getTransactionHistory(request);
 
     }
 
     @PreAuthorize("hasRole('USER')")
     @PostMapping(value = "/transaction/pages/v1")
-    public Flux<Transaction> getTransactionByPages(@RequestBody WeakHashMap<String,String> request) {
+    public Flux<Transaction> getTransactionByPages(@RequestBody WeakHashMap<String, String> request) {
         log.info(" getTransactionByPages method request ={}", request);
-        return  transactionService.getTransactionByPages(request);
+        return transactionService.getTransactionByPages(request);
 
     }
 
